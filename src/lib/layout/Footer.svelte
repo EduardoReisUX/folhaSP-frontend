50dc32b2e4b23305f18506d15c9bc8e9443c4ae2
EduardoReisUX <ducabruno09@hotmail.com>
2022-03-01 19:35:32 -0400

    add basic svelte transition

--- src/lib/layout/Footer.svelte
+++ src/lib/layout/Footer.svelte
@@ -1,22 +1,21 @@
 <script lang="ts">
-    import MdArrowUpward from 'svelte-icons/md/MdArrowUpward.svelte'
+	import MdArrowUpward from 'svelte-icons/md/MdArrowUpward.svelte';
 </script>
 
-<footer class="max-w-screen-xl mx-auto text-brand-white">
-    <div class="flex items-center justify-end text-sm bg-brand-blue">
-            <button class="flex gap-2 items-center px-4 py-2 ">
-                VOLTAR AO TOPO
-                <div class="h-6 w-6">
-                    <MdArrowUpward />
-                </div>
-            </button> 
-    </div>
-    <div class="bg-brand-blueDarker px-4 py-4">
-            <p class="text-xs">Copyright Folha de S. Paulo. Todos os direitos reservados.
-                É proibida a reprodução do conteúdo desta página em qualquer 
-                meio de comunicação, eletrônico ou impresso, sem autorização 
-                escrita da Folhapress 
-                (<a class="underline" href="/">pesquisa@folhapress.com.br</a>).
-            </p>
-    </div>
+<footer class="max-w-screen-xl mt-auto mx-auto text-brand-white">
+	<div class="flex items-center justify-end text-sm bg-brand-blue">
+		<button class="flex gap-2 items-center px-4 py-2 ">
+			VOLTAR AO TOPO
+			<div class="h-6 w-6">
+				<MdArrowUpward />
+			</div>
+		</button>
+	</div>
+	<div class="bg-brand-blueDarker px-4 py-4">
+		<p class="text-xs">
+			Copyright Folha de S. Paulo. Todos os direitos reservados. É proibida a reprodução do conteúdo
+			desta página em qualquer meio de comunicação, eletrônico ou impresso, sem autorização escrita
+			da Folhapress (<a class="underline" href="/">pesquisa@folhapress.com.br</a>).
+		</p>
+	</div>
 </footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,10 +2,12 @@
 	import Ad from '$lib/Ad.svelte';
 	import Carousel from '$lib/Carousel.svelte';
 	import DivoltisPorris from '$lib/DivoltisPorris.svelte';
-
 	import PaisisFilhis from '$lib/PaisisFilhis.svelte';
 	import SePirulita from '$lib/SePirulita.svelte';
 	import TopCacilds from '$lib/TopCacilds.svelte';
+	import { onMount } from 'svelte';
+
+	import { fly } from 'svelte/transition';
 
 	import type { divoltisType, paisisFilhisType, sePirulitaType, topCacildisType } from '.';
 
@@ -15,6 +17,9 @@
 	export let sePirulita: sePirulitaType;
 
 	let innerWidth = 0;
+
+	let pageLoaded = false;
+	onMount(() => (pageLoaded = true));
 </script>
 
 <svelte:window bind:innerWidth />
@@ -22,52 +27,65 @@
 <main
 	class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 max-w-screen-lg mx-auto px-4 my-8"
 >
-	<section class="flex flex-col gap-4 col-span-full">
-		<Carousel {innerWidth} />
-	</section>
+	{#if pageLoaded}
+		<section class="flex flex-col gap-4 col-span-full" transition:fly={{ y: -4, duration: 300 }}>
+			<Carousel {innerWidth} />
+		</section>
 
-	<section class="flex flex-col gap-4 col-span-full">
-		<p class="hidden md:block font-bold text-lg">top cacildis</p>
-		<div class="flex flex-col sm:flex-row gap-4 md:gap-10">
-			{#each topCacildis as props}
-				<TopCacilds {...props} />
-			{/each}
-		</div>
-	</section>
+		<section
+			class="flex flex-col gap-4 col-span-full"
+			transition:fly={{ y: -4, duration: 300, delay: 200 }}
+		>
+			<p class="hidden md:block font-bold text-lg">top cacildis</p>
+			<div class="flex flex-col sm:flex-row gap-4 md:gap-10">
+				{#each topCacildis as props}
+					<TopCacilds {...props} />
+				{/each}
+			</div>
+		</section>
 
-	<section class="flex flex-col gap-4 sm:col-span-2">
-		<p class="font-bold text-lg">divoltis porris</p>
-		<div class="flex flex-col gap-4 md:gap-10 sm:grid sm:grid-cols-2 sm:gap-8">
-			{#each divoltis as props}
-				<DivoltisPorris {...props} />
-			{/each}
-		</div>
-	</section>
+		<section
+			class="flex flex-col gap-4 sm:col-span-2"
+			transition:fly={{ y: -4, duration: 300, delay: 200 * 2 }}
+		>
+			<p class="font-bold text-lg">divoltis porris</p>
+			<div class="flex flex-col gap-4 md:gap-10 sm:grid sm:grid-cols-2 sm:gap-8">
+				{#each divoltis as props}
+					<DivoltisPorris {...props} />
+				{/each}
+			</div>
+		</section>
 
-	<section class="flex flex-col gap-4">
-		<p class="text-brand-blue text-lg">paisis, filhis, espiritis santis</p>
-		<div class="flex flex-col gap-4">
-			{#each paisisFilhis as title}
-				<PaisisFilhis {title} />
-			{/each}
-		</div>
-	</section>
+		<section class="flex flex-col gap-4" transition:fly={{ y: -4, duration: 300, delay: 200 * 3 }}>
+			<p class="text-brand-blue text-lg">paisis, filhis, espiritis santis</p>
+			<div class="flex flex-col gap-4">
+				{#each paisisFilhis as title}
+					<PaisisFilhis {title} />
+				{/each}
+			</div>
+		</section>
 
-	{#each sePirulita as props}
-		<div class="flex flex-col gap-4">
-			<SePirulita {...props} />
-		</div>
-	{/each}
+		{#each sePirulita as props}
+			<div class="flex flex-col gap-4" transition:fly={{ y: -4, duration: 300, delay: 200 * 4 }}>
+				<SePirulita {...props} />
+			</div>
+		{/each}
 
-	<section class="my-[4.5rem] md:my-7 md:col-start-3 md:row-start-3">
-		<Ad {innerWidth} />
-	</section>
+		<section class="my-[4.5rem] md:my-7 md:col-start-3 md:row-start-3">
+			<Ad {innerWidth} />
+		</section>
+	{/if}
 </main>
 
 <style global>
 	body {
-		min-height: 100vh;
 		background-color: rgb(243, 244, 246);
 		font-family: Arial, Helvetica, sans-serif;
+	}
+
+	body > div {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
 	}
 </style>
